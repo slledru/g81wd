e9bb809d52b8c5bc312c8b5c3d45c8e07ce28463
slledru <slledru@msn.com>
2018-02-07 15:43:22 -0700

    function

--- week1/function.js
+++ week1/function.js
@@ -4,5 +4,21 @@ var factorial = function fac(n) {
     : n * fac(n - 1);
 };
 
-console.log(factorial(5));
+//console.log(factorial(5));
 //console.log(fac(5));
+
+const square = (r) => r * r;
+//console.log(square(4));
+
+// accumulator pattern
+// function sphereVolAcc(radii) {
+//   let acc = [];
+//   for (let i = 0; i < radii.length; i++) {
+//     acc[i] = square(radii[i]);
+//   }
+//   return acc;
+// }
+
+
+const sphereVolAcc = (radii) => radii.map(square)
+console.log(sphereVolAcc([2,5,8]));
