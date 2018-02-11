e77fee1366ec3a5c4eb9c6bcd4d7158c17b1b875
slledru <slledru@msn.com>
2018-02-11 10:01:23 -0700

    end of week1

--- week1/function.js
+++ week1/function.js
@@ -21,4 +21,37 @@ const square = (r) => r * r;
 
 
 const sphereVolAcc = (radii) => radii.map(square)
-console.log(sphereVolAcc([2,5,8]));
+//console.log(sphereVolAcc([2,5,8]));
+
+const args = () => arguments.length // implied return
+
+//console.log(args(1,2,3,4));
+
+function total() {
+  var total = 0;
+  for (var i=0; i<arguments.length; i++) {
+    if (typeof arguments[i] === "number") {
+      total += arguments[i];
+    }
+  }
+  return total;
+}
+//console.log(total(1,4,3,'3'));
+
+const anotherArgs = () => {
+  //arguments.pop();  arguments does not implement pop() method
+  return arguments.length;
+}
+//console.log(anotherArgs());
+
+function power(base, exponent) {
+  console.log(exponent);
+  var exponent = (exponent || 2);
+  var result = 1;
+  for (var count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+}
+console.log(power(4, 0));
+console.log(power(5));
