67b1024afa7a9e82b7ff3d3ae9bfdf5eaa74fdec
slledru <slledru@msn.com>
2018-05-07 12:49:25 -0600

    react component

---
+++ week14/lesson_01/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Greeting from './Greeting'
+import Sidebar from './side_bar'
+
+const Wrapper = () => {
+  return (
+    <div>
+      <Greeting />
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Wrapper
---
+++ week14/lesson_01/side_bar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  <ul>
+    <li>
+      <a href="http://www.theonion.com">The Onion</a>
+    </li>
+  </ul>
+}
+
+export default Sidebar
