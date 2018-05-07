9008d9f126b8abf4b91d239ac12107d1bf7173a7
slledru <slledru@msn.com>
2018-05-07 13:25:23 -0600

    header and footer

---
+++ week14/lesson_02/cart_header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CartHeader = () => {
+  <header className="row">
+    <nav>
+      <div className="container">
+        <div className="nav-wrapper">
+          <div className="col s12">
+            <a href="/" className="brand-logo">Shopping Cart</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+}
